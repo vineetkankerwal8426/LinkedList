1bf60b4015625a6f4bf068b164ce86c9bc1447d0
Vineet Kumar Kankerwal <codevkankerwal@gmail.com>
2023-10-13 12:10:10 +0530

    algo for rotating linked list k times

---
+++ rotate_linked_list.cpp
@@ -0,0 +1,25 @@
+//Given the head of a linked list, rotate the list to the right by k places.
+
+//Input: head = [1,2,3,4,5], k = 2  ,, Output: [4,5,1,2,3]
+
+
+
+ListNode* rotateRight(ListNode* head, int k) {
+        if(head == nullptr || head->next == nullptr || k <=0){
+            return head;
+        }
+        ListNode * last = head;
+        int l = 1;
+        while(last->next){     //find the number of nodes in list and point last pointer to the tail;
+            last = last->next;
+            l++;
+        }
+        last->next = head;
+        ListNode * newLast = head;
+        for(int i =0;i<l-(k%l)-1;i++){   // number of rotations will be k%length;
+            newLast = newLast->next;
+        }
+        head = newLast->next ;    
+        newLast->next = nullptr;
+        return head;
+    }
